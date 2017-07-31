c75cffd0de47b40eccfd76b4584e4374ebda8344
reza.cse08 <reza.cse08@gmail.com>
2017-07-31 10:48:34 +0600

    first commit from local

---
+++ SingletonDesignPatternDemo/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;   
+using System.Threading.Tasks;
+
+namespace SingletonDesignPatternDemo
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Parallel.Invoke(
+                PrintCounterForFirst,
+                PrintCounterForSecond
+               );
+
+            Console.ReadLine();
+        }
+
+        private static void PrintCounterForFirst()
+        {
+            var firstObject = Singleton.GetInstance;
+            firstObject.PrintObjectCounter("Counter for first object");
+        }
+
+        private static void PrintCounterForSecond()
+        {
+            var secondObject = Singleton.GetInstance;
+            secondObject.PrintObjectCounter("Counter for first object");
+        }
+    }
+}
---
+++ SingletonDesignPatternDemo/Singleton.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+
+namespace SingletonDesignPatternDemo
+{
+    public sealed class Singleton
+    {
+
+        //to check object creation
+        private readonly int _counter;
+        private Singleton()
+        {
+            Console.WriteLine("mumber of instance created : " + ++_counter);
+        }
+
+
+        // lazy initialization for thread safety    
+        private static readonly object Obj = new object();
+        private static Singleton _instance;
+        public static Singleton GetInstance
+        {
+            get
+            {
+                //double check
+                if (_instance != null)
+                    return _instance;
+
+                lock (Obj)
+                {
+                    if (_instance == null)
+                        _instance = new Singleton();
+                }
+
+                return _instance;
+            }
+        }
+
+
+        //// eger initailization 
+        //public static Singleton GetInstance { get; } = new Singleton();
+
+
+        ////lazy initialization with Lazy  
+        //private static readonly Lazy<Singleton> _instance = new Lazy<Singleton>(() => new Singleton());
+        //public static Singleton GetInstance => _instance.Value;
+
+        public void PrintObjectCounter(string message)
+        {
+            Console.WriteLine(message + ": " + _counter);
+        }
+                                                             
+    }
+}
